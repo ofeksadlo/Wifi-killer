837eb5cfe9f8fb8a05c14fd79822f47f8baf4fbb
ofeksadlo <55595248+ofeksadlo@users.noreply.github.com>
2019-10-05 15:01:43 +0100

    Update ClientSideWifiKillerClass.Designer.cs
    Changed background to black and fore color to white.

--- ClientSideWifiKiller/ClientSideWifiKillerClass.Designer.cs
+++ ClientSideWifiKiller/ClientSideWifiKillerClass.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace gaverProject
+namespace gaverProject
 {
     partial class WifiKiller
     {
@@ -36,11 +36,14 @@
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 24);
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(215, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "מכבה אינטרנט בלפטופ";
             // 
@@ -49,19 +52,19 @@
             this.timer.Interval = 1000;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
-            // miniOfekHelper
+            // WifiKiller
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(258, 53);
             this.ControlBox = false;
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.Color.MistyRose;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "miniOfekHelper";
+            this.Name = "WifiKiller";
             this.Opacity = 0D;
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
